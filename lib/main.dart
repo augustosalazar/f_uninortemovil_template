826aa186b2a34d765712929469948b386c547c18
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2021-08-23 17:20:13 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter template',
       theme: MyTheme
           .ligthTheme, // Apply the theme referenced by the theme property on
       // MaterialApp
